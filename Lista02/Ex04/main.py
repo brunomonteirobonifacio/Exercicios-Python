13e1922835287d0f212a77bb23fa397571414187
Bruno <brunombonifacio@gmail.com>
2024-03-15 10:35:26 -0300

    Adicionado Ex04 da Lista02

---
+++ Lista02/Ex04/main.py
@@ -0,0 +1,6 @@
+valor1 = int(input('Digite o valor de x: '))
+valor2 = int(input('Digite o valor de y: '))
+
+resultado = (valor1 ** 2 + valor2 ** 2) / (valor1 - valor2)
+
+print(f'Resultado: {resultado:.2f}')
